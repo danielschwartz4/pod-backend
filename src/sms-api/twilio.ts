eb4f2b37f78e441ff772547164338a7463d61b69
Daniel <schwartzray8@gmail.com>
2022-04-16 13:33:26 -0400

    twilio functional

--- src/sms-api/twilio.ts
+++ src/sms-api/twilio.ts
@@ -1,9 +1,10 @@
 import bodyParser from "body-parser";
-import pino from "express-pino-logger";
-import { Twilio } from "twilio";
+import cors from "cors";
 import express from "express";
+import pino from "express-pino-logger";
+require("dotenv").config();
 
-const app = express();
+import { Twilio } from "twilio";
 
 // Twilio client
 let twilioClient: Twilio;
@@ -14,14 +15,23 @@ if (process.env.TWILIO_ACCOUNT_SID && process.env.TWILIO_AUTH_TOKEN) {
   );
 }
 
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    // credentials: true,
+  })
+);
+
 // Add body parser for Twilio
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(pino);
-
-app.set("port", 4001);
+// app.use(pino);
 
 app.post("/api/messages", (req, res) => {
+  console.log("text message");
+  console.log(req.body);
   res.header("Content-Type", "application/json");
   twilioClient.messages
     .create({
@@ -38,12 +48,27 @@ app.post("/api/messages", (req, res) => {
     });
 });
 
-app.listen(app.get("port"));
+app.get("/api/hello", (req, res) => {
+  console.log("hello");
+  res.send({ "Hello World": "Hello World" });
+});
 
-// fetch("/api/messages", {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   body: JSON.stringify(this.state.message),
-// });
+app.listen(4001, () => {
+  console.log("server started on port 4001");
+});
+
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// console.log("HELELOEOOO");
+// console.log(process.env.TWILIO_ACCOUNT_SID);
+// const client = require("twilio")(accountSid, authToken);
+
+// client.messages
+//   .create({
+//     messagingServiceSid: "MG6fe5a438dbe492eeb092926d2aaeee74",
+//     // from: process.env.TWILIO_PHONE_NUMBER,
+//     to: "+12173817277",
+//     body: "hello!",
+//   })
+//   .then((message: any) => console.log(message.sid))
+//   .done();

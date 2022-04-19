66f793b2d5b09424d7636b596b540c26b8f2ddde
Daniel <schwartzray8@gmail.com>
2022-04-19 01:04:15 -0400

    cors

--- src/sms-api/twilio.ts
+++ src/sms-api/twilio.ts
@@ -18,7 +18,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    // origin: ["http://localhost:3000"],
+    origin: process.env.VERCEL_APP,
     // credentials: true,
   })
 );

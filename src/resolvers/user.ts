d14afa7b42d6b9fabb40137595f14817f874ae55
Daniel <schwartzray8@gmail.com>
2022-05-26 19:00:04 -0500

    rm getConnection

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,6 @@
 import argon2 from "argon2";
 import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
-import { getConnection, In } from "typeorm";
+import { In } from "typeorm";
 import { COOKIE_NAME } from "../constants";
 import { User } from "../entities/User";
 import { MyContext, UserResponse } from "../types";
